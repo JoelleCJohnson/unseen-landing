cb424f2c75c3fdea8cf44dd74ad9cb07fafe0728
Joelle  Johnson <joellej221@gmail.com>
2024-01-02 11:39:45 -0500

    Pulled footer in to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <Footer />
+    </>
   );
 }
 
