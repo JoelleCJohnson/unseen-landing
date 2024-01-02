93741f2dcf486947391388ade6a6b53470503985
Michael Fisher <mafish88@icloud.com>
2024-01-02 12:42:09 -0500

    add image

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,30 +4,48 @@ import Form from "react-bootstrap/Form";
 export default function LeadForm() {
   return (
     <>
-    <h1 style={{margin: "100px" }}>Welcome to Unseen</h1>
+      <div style={{ display: "flex"}}>
+        <div>
+          <h1 style={{ margin: "100px" }}>Welcome to Unseen</h1>
 
-      <p>The first fully accessible mobile dating app for the visually impaired and blind</p>
-      <Form style={{width: "50vw"}}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Full Name</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+          <p>
+            The first fully accessible mobile dating app for the visually
+            impaired and blind
+          </p>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-
-        <Form.Text className="text-muted">
-          We'll never share your phone number with anyone else.
-        </Form.Text>
-
-        <br />
-
-        <Button variant="primary" type="submit">
-          Join the Waitlist
-        </Button>
-      </Form>
+          <Form style={{ width: "50vw" }}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Full Name</Form.Label>
+              <Form.Control placeholder="Enter full name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control type="number" placeholder="Enter phone number" />
+              <Form.Text className="text-muted">
+                We'll never share your phone number with anyone else.
+              </Form.Text>
+            </Form.Group>
+            <br />
+            <Form.Text className="text-muted">
+              By clicking 'Join Now', you agree to our Terms of Service and
+              Privacy Policy.
+            </Form.Text>
+            <br />
+            <Button variant="primary" type="submit">
+              Join the Waitlist
+            </Button>
+            <br />
+          </Form>
+        </div>
+        <div style={{display: "flex", justifyContent: "center", alignContent: "center", backgroundColor: "lightblue"}}>
+          <img
+            class="mockup_v2__device_img"
+            src="https://unicorn-cdn.b-cdn.net/f8c4871c-db7a-4f44-acb0-6e4854a590ed/apple_iphone_se_2020_black.png"
+            alt="device"
+            style={{ display: "initial;", width: "25vw", height: "auto" }}
+          ></img>
+        </div>
+      </div>
     </>
   );
 }
