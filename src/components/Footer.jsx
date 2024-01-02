c9b70779835536d8ad7bf826510ac6b503f24203
Michael Fisher <mafish88@icloud.com>
2024-01-02 11:29:58 -0500

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p> © 2024 Unseen - Making dating accessible for everyone </p>
+        </footer>
+
+
+    )
+
+
+
+}
