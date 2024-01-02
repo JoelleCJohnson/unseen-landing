fe06027618b06b94712913692990aa06a558eca3
Joelle  Johnson <joellej221@gmail.com>
2024-01-02 11:58:39 -0500

    Deleted old irrelevant code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,16 +36,3 @@ export default function Header() {
         );
     }
 
-    //     <header>
-    //         <h1>Unseen</h1>
-    //         <ul>
-    //             {/* hrefs will need to be updated once we have pages for these buttons */}
-    //             <li><a href="#home" >Home</a></li>
-    //             <li><a href="#features">Features</a></li>
-    //             <li><a href="#pricing">Pricing</a></li>
-    //             <li><a href="#contact"></a>Contact Us</li>
-    //             <li><button onClick={handleIOSDownload}>Download in the App Store</button></li>
-    //             <li><button onClick={handleAndroidDownload}>Download in the play Store</button></li>
-    //         </ul>
-    //     </header>
-    // )
